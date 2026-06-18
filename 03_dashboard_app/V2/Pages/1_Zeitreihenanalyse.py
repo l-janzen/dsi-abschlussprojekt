1f462dd8c44dca8d2262ce2387ead730ca7f0ec4
h4-4nh <haanhhanoitran@gmail.com>
2026-06-18 15:01:59 +0200

    1_Zeitreihenanalyse got functional charts, created  functions that helps with chart creation and naming

--- 03_dashboard_app/V2/Pages/1_Zeitreihenanalyse.py
+++ 03_dashboard_app/V2/Pages/1_Zeitreihenanalyse.py
@@ -20,33 +20,62 @@ st.set_page_config(
 )
 
 ################################################################################
+##functions
 
-def country(selected_stocks):
+#helps with creating pills
+def pill_list(option):
     item_list = []
-    for items in selected_stocks:
+    for items in option:
         item_list.append(items)
     return item_list
 
-
-def create_chart(df, selected_stocks):
+#helps with creating chart with different y values
+def create_chart(df, selected_options, x_axis = "timestamp", y_axis = "Wert", serie ="Serie"):
+    x_axis = x_axis + ":T"
+    y_axis = y_axis
+    serie = serie
     return (
         alt.Chart(df)
         .transform_fold(
-            country(selected_stocks),
-            as_=["series", "value"]
+            pill_list(selected_options),
+            as_= [serie ,y_axis]
         )
         .mark_line()
         .encode(
-            x="timestamp:T",
-            y="value:Q",
-            color="series:N",
-            tooltip=["timestamp:T", "series:N", "value:Q"]
+            x= x_axis,
+            y=y_axis + ":Q",
+            color= serie + ":N",
+            tooltip=[x_axis, serie + ":N", y_axis+ ":Q"]
         )
         .interactive()
     )
 
+chart_parameter = {
+    "Serie" : "Serie:T"
 
-###############################################################################
+} 
+
+
+##Data loading
+@st.cache_data
+def load_data():
+    return  pd.read_csv(r"..\dsi-abschlussprojekt\01_public_health_analysis\data\processed\germany_hypertension_public_health_2000_2019.csv", 
+    parse_dates=["Year"],
+    date_format="%Y"
+    )
+data_year = load_data()
+df_hyper_year = data_year.copy()
+
+@st.cache_data
+def load_data2():
+    return  pd.read_csv(r"..\dsi-abschlussprojekt\01_public_health_analysis\data\raw\hypertension-adults-30-79.csv", 
+    parse_dates=["Year"],
+    date_format="%Y"
+    )
+data_country = load_data2()
+df_country = data_country.copy()
+
+################################################################################
 st.title("Entwicklung von Hypertonie")
 st.subheader(":hearts:")
 #####
@@ -67,13 +96,19 @@ df = pd.DataFrame({
 })
 
 
-selected_stocks = True
 
-st.write(df)
-
+st.write(df_hyper_year)
+#schaut nach min und max Datum
 first_date = df["timestamp"].min().to_pydatetime()
 st.write(first_date)
 last_date = df["timestamp"].max().to_pydatetime()
+
+#schaut für df_hyper
+
+first_date_h = df_hyper_year["Year"].min().to_pydatetime()
+st.write(first_date)
+last_date_h = df_hyper_year["Year"].max().to_pydatetime()
+
 
 
 ##########################################
@@ -81,8 +116,13 @@ last_date = df["timestamp"].max().to_pydatetime()
 st.sidebar.subheader("Filtereinstellung")
 st.sidebar.write("Hier kannst du die Graphen filtern")
 
-#fügt Aktiennamen in die Seitenleiste als pills und filtert duplikate
-selected_stocks = st.sidebar.pills("Auswahl", data.columns[1:].drop_duplicates(), selection_mode ="multi")
+#fügt die Achsenamen in die Seitenleiste als pills und filtert duplikate
+selected_options = st.sidebar.pills("Auswahl", data.columns[1:].drop_duplicates(), selection_mode ="multi")
+
+
+#Geschlechtfilterung
+
+selected_gender = st.sidebar.pills("Auswahl Geschlecht", data_year.columns[3:6].drop_duplicates(), selection_mode ="multi")
 
 #Zeiteinstelllung
 
@@ -90,7 +130,7 @@ selected_stocks = st.sidebar.pills("Auswahl", data.columns[1:].drop_duplicates()
 
 #with st.sidebar.popover("Zeit-Einstellung"):
 #    date_range = (first_date, last_date)
-#    if selected_stocks:
+#    if selected_options:
 #        selected_date_range = st.slider(
 #            "Wähle deinen Zeitraum",
 #            min_value = first_date,
@@ -105,7 +145,7 @@ selected_stocks = st.sidebar.pills("Auswahl", data.columns[1:].drop_duplicates()
 #        selected_date_range = False
 #        teste = True
 
-if selected_stocks:
+if selected_options:
     teste = False
 else:
     selected_date_range = False
@@ -123,18 +163,53 @@ selected_date_range = st.sidebar.slider(
     key = "hallo3"
 )
 
+###############################
+#schaut ob eine Pill ausgewählt wird
 
-st.sidebar.write(selected_stocks)
+if selected_gender:
+    teste = False
+else:
+    selected_date_range_h = False
+    teste = True
+
+#Zeitfilterung
+
+years = pd.date_range(
+    first_date_h,
+    last_date_h,
+    freq="YS"  # Year Start
+)
+
+date_range_h = (first_date_h, last_date_h)
+selected_date_range_h = st.sidebar.select_slider(
+    "Wähle deinen Zeitraum",
+    options=years,
+    format_func=lambda x: x.strftime("%Y"), #stellt sicher, dass der slider in 1 Wochen schritten arbeitet
+    value=[years[0], years[-1] ],
+    disabled = teste,
+    key = "hyper_year_range"
+)
+
+
+##########################################
+st.sidebar.write(selected_options)
 
 
 
 #Daterange Implementierung
-if selected_stocks:
+if selected_options:
     df = df[
         (df["timestamp"]>=selected_date_range[0]) &
         (df["timestamp"]<=selected_date_range[1])
     ]
 
+
+#Daterange Implementierung für selected gender
+if selected_gender:
+    df_hyper_year = df_hyper_year[
+        (df_hyper_year["Year"]>=selected_date_range_h[0]) &
+        (df_hyper_year["Year"]<=selected_date_range_h[1])
+    ]
 
 
 
@@ -163,8 +238,8 @@ if selected_date_range:
 ################################################
 
 
-if selected_stocks:
-    chart = create_chart(df, selected_stocks)
+if selected_options:
+    chart = create_chart(df, selected_options)
 
     tab1, tab2 = st.tabs(
         ["Streamlit theme (default)", "Altair native theme"]
@@ -180,7 +255,35 @@ else:
 
 
 
+if selected_gender:
+    chart_hyper = create_chart(df_hyper_year, selected_gender, x_axis = "Year", y_axis = "Prävalenz", serie ="Serie")
+
+    tab3, tab4 = st.tabs(
+        ["Streamlit theme (default)", "Altair native theme"]
+    )
+    chart_hyper = chart_hyper.mark_circle(size=80) + chart_hyper
+    with tab3:
+        st.altair_chart(chart_hyper, theme="streamlit", use_container_width=True)
+
+    with tab4:
+        st.altair_chart(chart_hyper, theme=None, use_container_width=True)
+
+
+
+
+    
+else:
+    st.write("Wähle ein Geschlecht aus")
+
+
+
+
+
 #################################################################
 
 
 st.write(df[["value a", "value b"]].describe())
+
+
+st.subheader("Zusammenfassung")
+
